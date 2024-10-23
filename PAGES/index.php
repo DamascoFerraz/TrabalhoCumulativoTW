b456c1093d8c92ec882acc9e2f0b9d0664a14f69
Faaler <depaularafaelvictor@gmail.com>
2024-10-23 08:48:53 -0300

    deixando a pagina de login responsiva *em desenvolviento*

--- PAGES/index.php
+++ PAGES/index.php
@@ -11,79 +11,102 @@
 <body>
     <?php
         session_start();
-        //checa se o usuario ja esta logado, se sim, entra na pagina principal
         if(isset($_SESSION['user'])){
             header("Location:mainpage.php");
             exit;
         }
 
-        //checa se tem uma mensagem de retorno
-        //  aqui utilizamos variavel $_GET que é inserida no url
-        //      exemplo:                url.com?[nome da var]=[valor da var]
-        //                              (var fica salva como $_GET['nome da var'])
-        //      exemplo do projeto:     index.php?return=Campo_nao_preenchido
-        //                              (var fica salva como $_GET['return'])
         if(isset($_GET['return'])){
-            //metodo para transfirir a tal mensagem de retorno em uma variavel js
-            // o codigo abaixo escreve:
-            //      <script> var returne = 'mensagem de retorno'; </script>
-            //                              ^~~~~~~~~~~~~~~~~~~^-> definida pela var php $_GET['return']
             echo(str_replace("#","'","<script>var returne = #".$_GET['return']."#;</script>"));
         }
     ?>
 
+    <!-- Header responsivo com Bootstrap -->
     <header>
-        <nav>
-        <h1>MyHappyween</h1>
-        <br>
-            <a href="index.php">Sobre nós</a>
-            <a href="mainpage.php">Posts</a>
-            <a href="contato.php">Contato</a>
+        <nav class='navbar navbar-expand-lg'>
+            <div>
+                <a id='navbarlink' href="#">MyHappyween</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php">Sobre nós</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="mainpage.php">Posts</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contato.php">Contato</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>
     </header>
 
-    <nav class="form-container">
-        <!-- form de login -->
-        <form action="../PHP/form_log_handler.inc.php" method="post">
-            <label for="email">E-mail:</label>
-            <input type="text" name="email" id="inp-email-login" required><br>
-
-            <label for="pwd">Senha:</label>
-            <input type="password" name="pwd" id="inp-pwd-login" required><br>
-
-            <button type="submit">Log in</button>
-        </form>
-        <!-- form de criação de conta -->
-        <form action="../PHP/form_sing_handler.inc.php" method="POST">
-            <label for="name">Nome:</label>
-            <input type="text" name="name" id="inp-name-singin" required> <br>
-
-            <label for="pwd">Senha:</label>
-            <input type="password" name="pwd" id="inp-pwd-singin" required> <br>
-
-            <label for="email">E-mail:</label>
-            <input type="text" name="email" id="inp-email-singin" required>
-
-            <button type="submit">Sing in</button>
-        </form>
+    <!-- Formulários responsivos -->
+    <div class="container mt-5">
+        <div class="row">
+            <!-- Form de login -->
+            <div class="col-md-6 mb-4">
+                <div class="card p-4">
+                    <nav class='form-containe'>
+                    <form action="../PHP/form_log_handler.inc.php" method="post">
+                        <div class="mb-3">
+                            <label for="email" class="form-label">E-mail:</label>
+                            <input type="text" class="form-control" name="email" id="inp-email-login" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="pwd" class="form-label">Senha:</label>
+                            <input type="password" class="form-control" name="pwd" id="inp-pwd-login" required>
+                        </div>
+                        <button type="submit" class="btn btn-warning w-100">Log in</button>
+                    </form>
     </nav>
-    
-    <!-- DIALOG COM MENSAGEM DE RETORNO DAS PAGINAS PHP -->
-        <dialog id="return_dialog" class="modal">
-            <?php
-                if(isset($_GET['return'])){
-                    // invoca elementos html com conteudo da variavel de retorno (substituindo underlines '_' por espaços)
-                    echo "<h2>message</h2><hr>";
-                    echo(str_replace("_","&nbsp","{$_GET['return']}"));
-                    echo "<hr><button onclick='modal.close()'>Fechar</button>";
-                };
-            ?>
-        </dialog>
+                </div>
+            </div>
+            
+            <!-- Form de criação de conta -->
+            <div class="col-md-6 mb-4">
+                <div class="card p-4">
+                    <nav class='form-container'>
+                    <form class='forms' action="../PHP/form_sing_handler.inc.php" method="POST">
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Nome:</label>
+                            <input type="text" class="form-control" name="name" id="inp-name-singin" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="pwd" class="form-label">Senha:</label>
+                            <input type="password" class="form-control" name="pwd" id="inp-pwd-singin" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">E-mail:</label>
+                            <input type="text" class="form-control" name="email" id="inp-email-singin" required>
+                        </div>
+                        <button type="submit" class="btn btn-warning w-100">Sing in</button>
+                    </form>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- DIALOG COM MENSAGEM DE RETORNO -->
+    <dialog id="return_dialog" class="modal">
+        <?php
+            if(isset($_GET['return'])){
+                echo "<h2>message</h2><hr>";
+                echo(str_replace("_","&nbsp","{$_GET['return']}"));
+                echo "<hr><button class='btn btn-danger' onclick='modal.close()'>Fechar</button>";
+            }
+        ?>
+    </dialog>
 
     <!-- SCRIPT PARA MOSTRAR O DIALOG -->
     <script>
         const modal = document.getElementById("return_dialog");
-        // se existir uma mensagem de retorno, exibe o dialogo/modal
         if(typeof returne != 'undefined'){
             modal.showModal();
         }
